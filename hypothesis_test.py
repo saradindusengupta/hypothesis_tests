57a982f254b95d3742ba858f0683209a361fc3a5
Saradindu Sengupta <saradindu.mi1@iiitmk.ac.in>
2020-05-10 10:27:38 +0530

    Added initial commit
    Raw code for each tests

---
+++ hypothesis_test.py
@@ -0,0 +1,181 @@
+
+# Example of the Shapiro-Wilk Normality Test
+from scipy.stats import shapiro
+data = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+stat, p = shapiro(data)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably Gaussian')
+else:
+	print('Probably not Gaussian')
+1
+2
+3
+# Example of the D'Agostino's K^2 Normality Test
+from scipy.stats import normaltest
+data = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+stat, p = normaltest(data)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably Gaussian')
+else:
+	print('Probably not Gaussian')
+
+# Example of the Anderson-Darling Normality Test
+from scipy.stats import anderson
+data = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+result = anderson(data)
+print('stat=%.3f' % (result.statistic))
+for i in range(len(result.critical_values)):
+	sl, cv = result.significance_level[i], result.critical_values[i]
+	if result.statistic < cv:
+		print('Probably Gaussian at the %.1f%% level' % (sl))
+	else:
+		print('Probably not Gaussian at the %.1f%% level' % (sl))
+  
+  # Example of the Pearson's Correlation test
+from scipy.stats import pearsonr
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [0.353, 3.517, 0.125, -7.545, -0.555, -1.536, 3.350, -1.578, -3.537, -1.579]
+stat, p = pearsonr(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+ 
+ # Example of the Spearman's Rank Correlation Test
+from scipy.stats import spearmanr
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [0.353, 3.517, 0.125, -7.545, -0.555, -1.536, 3.350, -1.578, -3.537, -1.579]
+stat, p = spearmanr(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+ 
+# Example of the Kendall's Rank Correlation Test
+from scipy.stats import kendalltau
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [0.353, 3.517, 0.125, -7.545, -0.555, -1.536, 3.350, -1.578, -3.537, -1.579]
+stat, p = kendalltau(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+ 
+ 
+# Example of the Chi-Squared Test
+from scipy.stats import chi2_contingency
+table = [[10, 20, 30],[6,  9,  17]]
+stat, p, dof, expected = chi2_contingency(table)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+ 
+# Example of the Augmented Dickey-Fuller unit root test
+from statsmodels.tsa.stattools import adfuller
+data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+stat, p, lags, obs, crit, t = adfuller(data)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably not Stationary')
+else:
+	print('Probably Stationary')
+ 
+# Example of the Kwiatkowski-Phillips-Schmidt-Shin test
+from statsmodels.tsa.stattools import kpss
+data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+stat, p, lags, crit = kpss(data)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably not Stationary')
+else:
+	print('Probably Stationary')
+ 
+# Example of the Student's t-test
+from scipy.stats import ttest_ind
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = ttest_ind(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+ 
+# Example of the Paired Student's t-test
+from scipy.stats import ttest_rel
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = ttest_rel(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+ 
+
+# Example of the Analysis of Variance Test
+from scipy.stats import f_oneway
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+data3 = [-0.208, 0.696, 0.928, -1.148, -0.213, 0.229, 0.137, 0.269, -0.870, -1.204]
+stat, p = f_oneway(data1, data2, data3)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+# Example of the Mann-Whitney U Test
+from scipy.stats import mannwhitneyu
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = mannwhitneyu(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+ 
+from scipy.stats import wilcoxon
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = wilcoxon(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+ 
+ 
+# Example of the Kruskal-Wallis H Test
+from scipy.stats import kruskal
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = kruskal(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+ 
+# Example of the Friedman Test
+from scipy.stats import friedmanchisquare
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+data3 = [-0.208, 0.696, 0.928, -1.148, -0.213, 0.229, 0.137, 0.269, -0.870, -1.204]
+stat, p = friedmanchisquare(data1, data2, data3)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+ 
